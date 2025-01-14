57fdce9251c997a411f19e1aa197881f241ab450
arnas <a.rumbavicius@gmail.com>
2025-01-14 17:25:07 +0100

    Moved check_diff(WIP) into sample_file_scripts. And added file_path to sample_sheet

--- workflow/scripts/sample_file_scripts.py
+++ workflow/scripts/sample_file_scripts.py
@@ -8,17 +8,15 @@ import yaml
 
 from fetch_data import get_meta_data, get_sra_accessions
 
-#JSON_LOCATION = "/tmp/samples.json"
 
 class SampleFileScripts:
-    sample_sheet = None
-    paired_end = None
     def __init__(self, config):
         if config["sample_sheet"] is None or config["sample_sheet"] == '':
             self.sample_sheet = "config/samples.csv"
         else:
             self.sample_sheet = config["sample_sheet"]
         self.paired_end = config["paired_end"]
+        self.json_path = config["json_path"]
 
     def make_sample_info(self, json_path) -> dict[str:dict]:
         geo_accession_pattern = re.compile(r"^GSM[0-9]*$")
@@ -28,16 +26,20 @@ class SampleFileScripts:
         with open(self.sample_sheet, "r") as file:
             sample_sheet = pd.read_csv(file, keep_default_na=False)
         for index, row in sample_sheet.iterrows():
-            sample = row.values[5]
-            if geo_accession_pattern.match(sample):
+            sample = ""
+            availability_type = ""
+            if row["file_path"] == "" and geo_accession_pattern.match(row["accession"]):
                 availability_type = "public"
-                geo_accessions.add(sample)
+                sample = row["accession"]
+                geo_accessions.add(row["accession"])
                 sample_info[availability_type][sample] = {}
-            else:
+            if row["file_path"]:
                 # Check if the included files exist
                 availability_type = "provided"
-                paths = sample.split(";")
+                paths = row["file_path"].split(";")
                 sample = f"{row['mark']}_{row['sample']}_{row['type']}_rep{row['replicate']}".lstrip("_")
+                if row["accession"]:
+                    sample = f"{row['accession']}_{sample}"
                 sample_info[availability_type][sample] = {}
                 for i, read in enumerate(paths):
                     path = pathlib.Path(read)
@@ -46,7 +48,7 @@ class SampleFileScripts:
                         "file_extension": "".join(path.suffixes),
                         "file_name": path.name.split("".join(path.suffixes))[0]#.split(f"_{i + 1}")[0]
                     }
-                    sample_info[availability_type][sample]['file_name'] = f"{row['mark']}_{row['sample']}_{row['type']}_rep{row['replicate']}".lstrip("_")
+                    sample_info[availability_type][sample]['file_name'] = sample
             sample_info[availability_type][sample].update({
                 "type": row["type"],
                 "sample": row["sample"],
@@ -60,16 +62,55 @@ class SampleFileScripts:
 
         json_dir = "/".join(json_path.split("/")[:-1]) + "/"
         if not os.path.exists(json_dir): os.makedirs(json_dir)
-        with open(json_path, "w") as outfile:
+        with open(self.json_path, "w") as outfile:
             outfile.write(json.dumps(sample_info, indent=4))
         return sample_info
 
     def is_paired_end(self) -> bool:
         return str(self.paired_end).lower() == "true"
 
+    def check_diff(self) -> bool:
+        samples_csv = open(self.sample_sheet)
+        json_file = open(self.json_path)
+        sample_sheet = pd.read_csv(samples_csv, keep_default_na=False); samples_csv.close()
+        samples_json = json.load(json_file); json_file.close()
+        samples_json = SampleFileScripts.__flatten_dict(samples_json)
+        print(samples_json)
+        if len(sample_sheet) != len(samples_json["public"]) + len(samples_json["provided"]): return False
+        for index, row in sample_sheet.iterrows():
+            if row["accession"] in samples_json["public"]:
+                # print(row["accession"], "in public")
+                for header in sample_sheet.head():
+                    if header == "accession": continue
+                    if not header in samples_json["public"][row["accession"]]: return False
+                    if not row[header] == samples_json["public"][row["accession"]][header]: return False
+                continue
+            if row["accession"] in samples_json["provided"]:
+
+                # print(row["accession"], "in provided")
+                continue
+            return False
+        return True
+
     @staticmethod
     def get_file_info(json_path):
         with open(json_path, "r") as file:
             json_data = json.load(file)
         return json_data
+
+    @staticmethod
+    def __flatten_dict(old_dict: dict) -> dict:
+        new_dict = {}
+        for key, value in old_dict.items():
+            new_dict = new_dict | value
+        return new_dict
+
+
+if __name__ == "__main__":
+    file = open("config/config.yml")
+    config = yaml.load(file, Loader=yaml.FullLoader)
+    file.close()
+    sfs = SampleFileScripts(config)
+    sfs.make_sample_info("")
+
 
