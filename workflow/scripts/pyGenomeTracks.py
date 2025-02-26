557affaf0345db99d917123aa3baadcbb8628607
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-02-26 15:36:11 +0100

    Remade pyGenomeTracks make_tracks_file script

--- workflow/scripts/pyGenomeTracks.py
+++ workflow/scripts/pyGenomeTracks.py
@@ -1,16 +1,21 @@
 from snakemake.script import snakemake
 from snakemake import shell
+from make_tracks_file import make_tracks
 import sys
 
 log = open(snakemake.log[0], "w")
 sys.stderr = log
 sys.stdout = log
 command = f"exec > {snakemake.log[0]} 2>&1"
-command += f" \nmake_tracks_file -f {snakemake.input['beds']} {snakemake.input['bigwigs']} -o {snakemake.output['tracks']}"
-command += f" \npython3 workflow/scripts/rename_tracks.py {snakemake.output.tracks}"
+
+
+options = {"bed": {}, "bigwig": {}}
+if snakemake.params.peak_type == "narrow":
+    options["bed"]["file_type"] = "narrow_peak"
 if snakemake.params.bigwig_max:
-    assert snakemake.params.bigwig_max.isnumeric(), "bigwig_max param must be numeric"
-    command += " -x " + snakemake.params.bigwig_max
-command += f" \npyGenomeTracks --tracks {snakemake.output.tracks} --region {snakemake.params.region} --outFileName {snakemake.output.plot} {snakemake.params.args}"
+    options["bigwig"]["max_value"] = snakemake.params.bigwig_max
+options["bed"]["show_labels"] = "false"
+make_tracks(snakemake.output.tracks, snakemake.input.bed, snakemake.input.bigwig, options)
+command = f" pyGenomeTracks --tracks {snakemake.output.tracks} --region {snakemake.params.region} --outFileName {snakemake.output.plot} {snakemake.params.args}"
 log.close()
 shell(command)
