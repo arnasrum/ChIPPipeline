4f06e92e92eb9e3e71a251e354df4e2e4f0dfff6
arnas <a.rumbavicius@gmail.com>
2025-03-07 23:22:52 +0100

    Moved out validating code to abstract class

--- workflow/scripts/trimmers/cutadapt.py
+++ workflow/scripts/trimmers/cutadapt.py
@@ -3,13 +3,8 @@ import os
 
 class Cutadapt(Trimmer):
     def trim(self, output_path: str, read1: str, read2:str =None, threads:int=1, args:str=None) -> str:
-        if not os.path.isfile(read1) and False:
-            raise FileNotFoundError(f"Provided file; {read1}, does not exist")
-        if read2 is not None and not os.path.isfile(read2) and False:
-            raise FileNotFoundError(f"Provided file; {read2}, does not exist")
-        file_name = os.path.basename(read1.split("/")[-1].split(".fastq")[0])
-        file_name = file_name.strip("_1")
-        file_name = file_name.strip("_2")
+
+        file_name = Trimmer.get_file_name(read1, read2)
         read_extensions = [".fastq"]
         if read2 is not None:
             file_name = os.path.commonprefix([file_name, os.path.basename(read2.split("/")[-1])])
--- workflow/scripts/trimmers/trimmomatic.py
+++ workflow/scripts/trimmers/trimmomatic.py
@@ -6,14 +6,7 @@ class Trimmomatic(Trimmer):
     run_options = None
 
     def trim(self, output_path, read1, read2=None, threads=1, args=None) -> str:
-        if not os.path.isfile(read1) and False:
-            raise FileNotFoundError(f"Provided file; {read1}, does not exist")
-        if read2 is not None and not os.path.isfile(read2) and False:
-            raise FileNotFoundError(f"Provided file; {read2}, does not exist")
-        file_name = os.path.basename(read1.split("/")[-1])
-        file_name = file_name.strip(".fastq")
-        file_name = file_name.strip("_1")
-        file_name = file_name.strip("_2")
+        file_name = Trimmer.get_file_name(read1, read2)
         read_extensions = [".fastq"]
         if read2 is not None:
             file_name = os.path.commonprefix([file_name, os.path.basename(read2.split("/")[-1])])
