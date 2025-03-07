1b65835ebf9ae3384dfa6e35af57095b9bdd8edc
arnas <a.rumbavicius@gmail.com>
2025-03-07 23:26:37 +0100

    Added type hints

--- workflow/scripts/aligners/aligner.py
+++ workflow/scripts/aligners/aligner.py
@@ -3,14 +3,14 @@ from abc import ABC, abstractmethod
 class Aligner(ABC):
 
     @abstractmethod
-    def align(self, index, read1, read2=None, threads=1, args=None) -> str:
+    def align(self, index: str, read1: str, read2: str=None, threads: int=1, args: str=None) -> str:
         pass
     @abstractmethod
     def build_index(self, index: str, output_prefix: str, threads=1, args=None) -> str:
         pass
     @abstractmethod
-    def get_index_output(self, prefix, genome) -> list[str]:
+    def get_index_output(self, prefix: str, genome: str) -> list[str]:
         pass
     @abstractmethod
-    def get_name(self):
+    def get_name(self) -> str:
         pass
