6d14fe4e979b86b19ac5b595b8be0eace1fbcc57
arnas <a.rumbavicius@gmail.com>
2025-03-07 23:23:50 +0100

    Moved out code

--- workflow/scripts/trimmers/fastp.py
+++ workflow/scripts/trimmers/fastp.py
@@ -3,16 +3,12 @@ import os
 
 class Fastp(Trimmer):
     def trim(self, output_path, read1, read2=None, threads=1, args=None):
-        if not os.path.isfile(read1) and False:
-            raise FileNotFoundError(f"Provided file; {read1}, does not exist")
-        if read2 is not None and not os.path.isfile(read2) and False:
-            raise FileNotFoundError(f"Provided file; {read2}, does not exist")
-        file_name = os.path.basename(read1.split("/")[-1])
+
+        file_name = Trimmer.get_file_name(read1, read2)
         read_extensions = [""]
         if read2 is not None:
             file_name = os.path.commonprefix([file_name, os.path.basename(read2.split("/")[-1])])
             read_extensions = ["_1.fastq", "_2.fastq"]
-        file_name = file_name.strip("_1" "_2")
 
         command = "fastp"
         command += f" -w {threads}"
