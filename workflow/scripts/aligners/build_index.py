c09a11e1dcd7a338333ff2ce3e7f03dec31ea14e
arnas <a.rumbavicius@gmail.com>
2025-03-07 23:23:36 +0100

    Added STAR wrapper support

--- workflow/scripts/aligners/build_index.py
+++ workflow/scripts/aligners/build_index.py
@@ -3,6 +3,7 @@ from snakemake import shell
 import os
 from bowtie2 import Bowtie2
 from bwa_mem2 import BwaMem2
+from workflow.scripts.aligners.star import STAR
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
@@ -10,6 +11,8 @@ if snakemake.config['aligner'] == "bowtie2":
     aligner = Bowtie2()
 elif snakemake.config['aligner'] == "bwa-mem2":
     aligner = BwaMem2()
+elif str(snakemake.config['aligner']).lower() == "star":
+    aligner = STAR()
 else:
    raise Exception(f"Unknown aligner {snakemake.config['aligner']}")
 prefix = str(os.path.commonprefix(snakemake.output).rstrip("."))
