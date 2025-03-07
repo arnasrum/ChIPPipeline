0c3067a48f78d003870357ba088d2a8ccf6382cf
arnas <a.rumbavicius@gmail.com>
2025-03-07 23:21:47 +0100

    Working STAR wrapper

--- workflow/scripts/aligners/star.py
+++ workflow/scripts/aligners/star.py
@@ -5,30 +5,30 @@ class STAR(Aligner):
     temp_dir = ""
 
     def align(self, index, read1, read2=None, threads=1, args=None) -> str:
-        command = "bwa-mem2 mem"
+        command = "STAR --readFilesType Fastx"
         if not read1:
             raise Exception("No read1 provided")
         if not index:
             raise Exception("No index provided")
         if args:
             command += " " + args
-        command += f" -t {threads}"
+        command += f" --genomeDir {index}"
+        command += f" --runThreadN {threads}"
         command += " " + index
-        command += f" {read1}"
+        command += f" --readFilesIn {read1}"
         if read2:
             command += f" {read2}"
+        command += " --outStd SAM"
         return command
     
     def build_index(self, index: str, output_prefix: str, threads=1, args=None) -> str:
         args = args if args else ""
-        command = "gzip -kfd \n"
-        command += f"STAR --runMode genomeGenerate --runThreadN {threads} --genomeDir {output_prefix} --genomeFastaFiles {index} {args}"
+        command = f"STAR --runMode genomeGenerate --runThreadN {threads} --genomeDir {output_prefix} --genomeFastaFiles {index} {args}"
         return command
 
     def get_index_output(self, prefix, genome) -> list[str]:
-        index_files = [f"{prefix.rstrip('/')}/{genome}.{ext}"
-             for ext in ["amb", "ann", "pac", "bwt.2bit.64", "0123"]
-        #multiext(RESULTS + "/star-index/SA", "", "index")
+        index_files = [f"{prefix.rstrip('/')}/{genome}/{file}"
+             for file in ["SA", "SAindex", "Genome"]
         ]
         return index_files
 
