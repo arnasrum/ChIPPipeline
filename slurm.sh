5ccbc8d819b6596ddfea436939be8ac17b9b8757
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-03-12 05:28:50 +0100

    Moved CLI arguments to default profile

--- slurm.sh
+++ slurm.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 # Job name:
-#SBATCH --job-name=Chip-Data-Analysis-Test
+#SBATCH --job-name=Chip-Data-Analysi
 # Project:
-#SBATCH --account=nn11067k
+#SBATCH --account=<account>
+#SBATCH --partition=<partition>
 #SBATCH --time=00:05:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4G
-#SBATCH --partition=normal
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 
 ## Do some work:
-apptainer exec -B /cluster container_name snakemake -c <num> --jobs <num> --use-conda --conda-frontend conda --ri
-~                                                                                                     
+apptainer exec -B /cluster container_name snakemake -c $SLURM_CPUS_PER_TASK
