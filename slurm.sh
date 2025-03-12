c6b015c809473a11a7d28a22677cd3084db89576
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-03-12 05:51:47 +0100

    Started accounting for memory

--- slurm.sh
+++ slurm.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Job name:
-#SBATCH --job-name=Chip-Data-Analysi
+#SBATCH --job-name=Chip-Data-Analysis
 # Project:
 #SBATCH --account=<account>
 #SBATCH --partition=<partition>
 #SBATCH --time=00:05:00
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem=60000MB
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 
 ## Do some work:
-apptainer exec -B /cluster container_name snakemake -c $SLURM_CPUS_PER_TASK
+apptainer exec -B /cluster container_name snakemake -c $SLURM_CPUS_PER_TASK --resources mem_mb=$SLURM_MEM_PER_NODE
