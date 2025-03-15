4f80473663329103540b76ce109d89ad1964ce6a
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-03-15 05:04:33 +0100

    Using default compression setting

--- workflow/scripts/handle_provided_files.py
+++ workflow/scripts/handle_provided_files.py
@@ -7,11 +7,11 @@ command = ""
 if snakemake.input[0][-3:] == ".gz":
     command = f"ln -sr {snakemake.input[0]} {snakemake.output[0]}"
 else:
-    command = f"gzip -kfc9 {snakemake.input[0]} > {snakemake.output[0]}"
+    command = f"gzip -kfc {snakemake.input[0]} > {snakemake.output[0]}"
 if len(snakemake.output) == 2:
     if snakemake.input[1][-3:] == ".gz":
         command += f"\nln -sr {snakemake.input[1]} {snakemake.output[1]}"
     else:
-        command += f"\ngzip -kfc9 {snakemake.input[1]} > {snakemake.output[1]}"
+        command += f"\ngzip -kfc {snakemake.input[1]} > {snakemake.output[1]}"
 
 shell(f"({command}) {log}")
