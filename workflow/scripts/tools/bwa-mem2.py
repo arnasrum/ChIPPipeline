348e0ee3eb104f034b3e0459604c22bb21a6c37b
arnas <a.rumbavicius@gmail.com>
2025-03-18 13:55:58 +0100

    Fixed bwa-mem2 wrapper

--- workflow/scripts/tools/bwa-mem2.py
+++ workflow/scripts/tools/bwa-mem2.py
@@ -3,13 +3,14 @@ from snakemake import shell
 from os import path
 
 
-log = snakemake.log_fmts(stdout=True, stderr=True)
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
 command = "bwa-mem2 mem"
 command += f" -t {snakemake.threads}"
 if snakemake.params['args']:
     command += f" {snakemake.params['args']}"
-command += f" -R @RG\tID:{snakemake.wildcards.sample}\tSM:{snakemake.wildcards.sample}"
-command += f" {path.commonpath(snakemake.input['genome_index'])}"
+command += f" -R '@RG\tID:{snakemake.wildcards.sample}\tSM:{snakemake.wildcards.sample}'"
+command += f" {path.commonprefix(snakemake.input['genome_index'])}".rstrip(".")
 command += " " + " ".join(snakemake.input['reads'])
 command += f" | samtools view -o {snakemake.output[0]} -"
-shell(f"echo 'Executing {command}'\n({command}) {log}")
+shell(f"(echo 'Executing: {command}'\n{command}) {log}")
