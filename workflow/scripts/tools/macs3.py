ccf4105966444d743b8429f3bb38a5ecd148191d
arnas <a.rumbavicius@gmail.com>
2025-03-19 15:51:24 +0100

    Rolled back to old method

---
+++ workflow/scripts/tools/macs3.py
@@ -0,0 +1,30 @@
+from snakemake.script import snakemake
+from snakemake import shell
+from os import path
+
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+out_dir = str(path.commonpath(snakemake.output))
+
+command = "macs3 callpeak"
+if snakemake.params['args']:
+    command += f" {snakemake.params['args']}"
+command += f" --outdir {out_dir}"
+if str(snakemake.config['paired_end']).lower() == "true":
+    command += f" -f BAMPE"
+else:
+    command += f" -f BAM"
+
+command += f" --tempdir {snakemake.resources['tmpdir']}"
+command += f" -t {" ".join(snakemake.input['treatment'])} "
+command += f" -n {snakemake.wildcards['sample']}"
+if snakemake.input['control']:
+    command += f" -c {" ".join(snakemake.input['control'])} "
+if snakemake.params['peak_type'] == "broad":
+    command += " --broad"
+    command += (f"\nmv {out_dir}/{snakemake.wildcards['sample']}_peaks.broadPeak " +
+     f"{out_dir}/{snakemake.wildcards['sample']}.bed")
+if snakemake.params['peak_type'] == "narrow":
+    command += (f"\nmv {out_dir}/{snakemake.wildcards['sample']}_peaks.narrowPeak " +
+                f"{out_dir}/{snakemake.wildcards['sample']}.bed")
+shell(f"({command}) {log}")
