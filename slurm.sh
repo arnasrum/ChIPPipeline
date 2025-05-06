3bbcf6db74e7cbb53604087e47f3f16245a4fb33
arnas <a.rumbavicius@gmail.com>
2025-05-06 22:42:32 +0200

    minor refactoring

--- slurm.sh
+++ slurm.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Job name:
-#SBATCH --job-name=Chip-Data-Analysis-Test
+#SBATCH --job-name=Chip-Data-Analysis-Pipeline
 # Project:
-#SBATCH --account=nn11067k
+#SBATCH --account=<account>
 #SBATCH --time=00:05:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem-per-cpu=4G
-#SBATCH --partition=normal
+#SBATCH --partition=<partition>
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 
 ## Do some work:
-apptainer exec -B /cluster container_name snakemake -c <num> --jobs <num> --use-conda --conda-frontend conda --ri
+apptainer exec -B </path_to_workdir> <container_name> snakemake -c <num>
 ~                                                                                                     
