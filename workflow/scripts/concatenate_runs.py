a9b6e1a1ab24b699ae54c5f265e18769a567f76f
arnas <a.rumbavicius@gmail.com>
2025-05-07 17:55:27 +0200

    Set down compression level

--- workflow/scripts/concatenate_runs.py
+++ workflow/scripts/concatenate_runs.py
@@ -6,10 +6,10 @@ log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 if len(snakemake.output) == 2:
     reads1 = " ".join(filter(lambda file: file.endswith("_1.fastq"), snakemake.input))
     reads2 = " ".join(filter(lambda file: file.endswith("_2.fastq"), snakemake.input))
-    command = f"cat {reads1} | gzip -fc9 - > {snakemake.output[0]}"
-    command += f"\ncat {reads2} | gzip -fc9 - > {snakemake.output[1]}"
+    command = f"cat {reads1} | gzip -fc - > {snakemake.output[0]}"
+    command += f"\ncat {reads2} | gzip -fc - > {snakemake.output[1]}"
 else:
     reads = " ".join(snakemake.input)
-    command = f"cat {reads} | gzip -fc9 - > {snakemake.output[0]}"
+    command = f"cat {reads} | gzip -fc - > {snakemake.output[0]}"
 
 shell(f"(echo 'Command: {command}'\n{command}) {log}")
