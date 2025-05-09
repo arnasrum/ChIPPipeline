581d7a834582d947cb034f351062523968468e6c
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-05-09 15:39:21 +0200

    peak_calling_input tests

--- tests/test_input_functions.py
+++ tests/test_input_functions.py
@@ -122,5 +122,79 @@ def test_alignment_input_base_case_single_end(mock_pipeline_config):
     ]
     assert alignment_input(file_name, results_path, mock_pipeline_config) == expected_outputs
 
+def test_peak_calling_input_base_case(mock_pipeline_config):
+    mock_pipeline_config.get_config_option.return_value = "mark_duplicates"
+    mock_pipeline_config.group_treatment_files.return_value = {
+        'antibody_sample_genome' : {
+            1: [
+                'GSM0000000_antibody_sample_treatment_genome1_rep1'
+            ],
+            2: [
+                'GSM0000000_antibody_sample_treatment_genome2_rep2'
+            ],
+        }
+    }
+    mock_pipeline_config.get_control_files.side_effect = lambda key: {
+        'GSM0000000_antibody_sample_treatment_genome1_rep1': ['control1'],
+        'GSM0000000_antibody_sample_treatment_genome2_rep2': ['control2']
+    }.get(key)
 
+    result_path = "results"
 
+    expected_outputs = {
+            'control': [f"results/mark_duplicates/control1.bam"],
+            'treatment': [f"results/mark_duplicates/GSM0000000_antibody_sample_treatment_genome1_rep1.bam"]
+        }
+    assert expected_outputs == peak_calling_input('GSM0000000_antibody_sample_treatment_genome1_rep1', result_path, mock_pipeline_config)
+    
+def test_peak_calling_input_treatment_pool(mock_pipeline_config):
+    mock_pipeline_config.get_config_option.return_value = "mark_duplicates"
+    mock_pipeline_config.group_treatment_files.return_value = {
+        'antibody_sample_genome' : {
+            1: [
+                'GSM0000000_antibody_sample_treatment_genome1_rep1',
+                'GSM0000002_antibody_sample_treatment_genome1_rep1'
+            ],
+            2: [
+                'GSM0000000_antibody_sample_treatment_genome2_rep2'
+            ],
+        }
+    }
+    mock_pipeline_config.get_control_files.side_effect = lambda key: {
+        'GSM0000000_antibody_sample_treatment_genome1_rep1': ['control1'],
+        'GSM0000000_antibody_sample_treatment_genome2_rep2': ['control2']
+    }.get(key)
+
+    result_path = "results"
+
+    expected_outputs = {
+            'control': ["results/mark_duplicates/control1.bam"],
+            'treatment': ["results/mark_duplicates/GSM0000000_antibody_sample_treatment_genome1_rep1.bam",
+                          f"results/mark_duplicates/GSM0000002_antibody_sample_treatment_genome1_rep1.bam"
+            ]
+        }
+    assert expected_outputs == peak_calling_input('GSM0000000_antibody_sample_treatment_genome1_rep1', result_path, mock_pipeline_config)
+
+def test_peak_calling_input_control_pool(mock_pipeline_config):
+    mock_pipeline_config.get_config_option.return_value = "mark_duplicates"
+    mock_pipeline_config.group_treatment_files.return_value = {
+        'antibody_sample_genome': {
+            1: [
+                'GSM0000000_antibody_sample_treatment_genome1_rep1',
+            ],
+            2: [
+                'GSM0000000_antibody_sample_treatment_genome2_rep2'
+            ],
+        }
+    }
+    mock_pipeline_config.get_control_files.side_effect = lambda key: {
+        'GSM0000000_antibody_sample_treatment_genome1_rep1': ['control1', 'control3'],
+        'GSM0000000_antibody_sample_treatment_genome2_rep2': ['control2']
+    }.get(key)
+    result_path = "results"
+
+    expected_outputs = {
+        'control': ["results/mark_duplicates/control1.bam", "results/mark_duplicates/control3.bam"],
+        'treatment': ["results/mark_duplicates/GSM0000000_antibody_sample_treatment_genome1_rep1.bam"]
+    }
+    assert expected_outputs == peak_calling_input('GSM0000000_antibody_sample_treatment_genome1_rep1', result_path, mock_pipeline_config)
