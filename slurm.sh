19def090117f2d22043c075681c9e53cc74eb090
Arnas Rumbavicius <a.rumbavicius@gmail.com>
2025-05-09 06:06:42 +0200

    Better Slurm script

--- slurm.sh
+++ slurm.sh
@@ -5,11 +5,11 @@
 # Project:
 #SBATCH --account=<account>
 #SBATCH --time=00:05:00
-#SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=4G
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=64000
 #SBATCH --partition=<partition>
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 
 ## Do some work:
-apptainer exec -B </path_to_workdir> <container_name> snakemake -c <num>
+apptainer exec -B </path_to_workdir> <container_name> snakemake -c $SLURM_CPUS_PER_TASK --resources mem_mb=$SLURM_MEM_PER_CPU
