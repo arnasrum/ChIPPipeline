72734b153f51e224b9dff3815b4cd55fc90a1b85
arnas <a.rumbavicius@gmail.com>
2025-05-14 07:04:45 +0200

    pooling for bigwigs

--- workflow/scripts/tools/bigwig_compare.py
+++ workflow/scripts/tools/bigwig_compare.py
@@ -1,18 +1,44 @@
 from snakemake.script import snakemake
+from uuid import uuid4
 from snakemake import shell
-from os import path
 
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
-command = "bigwigCompare"
-if len(snakemake.input) == 2:
-    command = f"bigwigCompare -b1 {snakemake.input['treatment']} -b2 {snakemake.input['control']} -o {snakemake.output}"
-    if snakemake.params["args"]:
-        command += " {args}"
-elif len(snakemake.input) == 1:
-    f"ln -sr {snakemake.input['treatment']} {snakemake.output}"
-else:
-    raise Exception("Something went wrong with the input function")
 
-command += "\necho Running: \"{command}\""
-shell(f"({command}) {log}")
+treatment_bigwig = None
+control_bigwig = None
+
+if len(snakemake.input['treatment']) == 1:
+    treatment_bigwig = (snakemake.input['treatment'][0])
+elif len(snakemake.input['treatment']) > 1:
+    current = snakemake.input['treatment'][0]
+    for next_bigwig in snakemake.input['treatment'][1:]:
+        tmp_treatment_bigwig = f"{snakemake.resources['tmpdir']}/{uuid4()}.bw"
+        shell(f"bigwigCompare -p {snakemake.threads} -b1 {current} -b2 {next_bigwig} -o {tmp_treatment_bigwig}")
+        current = tmp_treatment_bigwig
+    treatment_bigwig = tmp_treatment_bigwig
+else:
+    raise Exception("Something went wrong")
+
+if len(snakemake.input['control']) == 0:
+    pass
+elif len(snakemake.input['control']) == 1:
+    control_bigwig = (snakemake.input['control'][0])
+elif len(snakemake.input['control']) > 1:
+    current = snakemake.input['control'][0]
+    for next_bigwig in snakemake.input['treatment'][1:]:
+        tmp_control_bigwig = f"{snakemake.resources['tmpdir']}/{uuid4()}.bw"
+        shell(f"(bigwigCompare -p {snakemake.threads} -b1 {current} -b2 {next_bigwig} -o {tmp_control_bigwig}) {log}")
+        current = tmp_control_bigwig
+    control_bigwig = tmp_control_bigwig
+else:
+    raise Exception("Something went wrong")
+
+if treatment_bigwig and control_bigwig:
+    shell(f"(bigwigCompare -p {snakemake.threads} -b1 {treatment_bigwig} -b2 {control_bigwig} -o {snakemake.output}) {log}")
+    shell(f"rm {treatment_bigwig} {control_bigwig}")
+elif treatment_bigwig:
+    print(f"(mv {treatment_bigwig} {snakemake.output})")
+    shell(f"(mv {treatment_bigwig} {snakemake.output}) {log}")
+else:
+    raise Exception("Something went wront")
